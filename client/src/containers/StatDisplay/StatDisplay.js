81548f4ee546a1d5c3a1dca1261258c60ee5d61a
Tony Martinez <ajosephmartinez@gmail.com>
2018-10-08 20:52:25 -0700

    Display player names, start work on table to show exact stats, begin refactoring to use CSS in JS throughout project

--- client/src/containers/StatDisplay/StatDisplay.js
+++ client/src/containers/StatDisplay/StatDisplay.js
@@ -10,27 +10,43 @@ class StatDisplay extends Component {
   render() {
     console.log(this.props.searchPlayer);
     let countingStats = [];
-    let radar = null;
+    let stats = null;
+    let names;
     let statsA = this.props.searchPlayer.A.countingStats;
     let statsB = this.props.searchPlayer.B.countingStats;
     if (statsA !== null) {
       statsA.color = colors.blue;
       countingStats[0] = statsA;
+      console.log("name", statsA.NAME);
+      names = <h2 style={{ color: colors.blue }}>{statsA.NAME}</h2>;
     }
     if (statsB !== null) {
       statsB.color = colors.orange;
       countingStats[1] = statsB;
+      names = <h2 style={{ color: colors.orange }}>{statsB.NAME}</h2>;
+    }
+    if (statsA !== null && statsB !== null) {
+      names = (
+        <div>
+          <h2>
+            <span style={{ color: colors.blue }}> {statsA.NAME}</span>
+            <span style={{ color: colors.red }}> vs </span>
+            <span style={{ color: colors.orange }}>{statsB.NAME}</span>
+          </h2>
+        </div>
+      );
     }
     if (!(statsA == null && statsB == null)) {
-      radar = <AnimatedRadar countingStats={countingStats} />;
+      stats = (
+        <div>
+          {names}
+          <AnimatedRadar countingStats={countingStats} />
+          <StatsTable />
+        </div>
+      );
     }
 
-    return (
-      <div style={{ textAlign: "center" }}>
-        <StatsTable />
-        {radar}
-      </div>
-    );
+    return <div style={{ textAlign: "center" }}>{stats}</div>;
   }
 }
 
--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -1,6 +1,7 @@
 const colors = {
   blue: "#0095B3",
-  orange: "#FF921B"
+  orange: "#FF921B",
+  red: "#EF5A6D"
 };
 
 export default colors;
